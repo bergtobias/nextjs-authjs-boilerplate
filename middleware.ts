0f0a7b0e0d9bb8eb6257752f7345f1b2584d47fe
Tobias Berg <nilsdacke1@gmail.com>
2024-10-10 22:46:20 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const user = req.auth?.user;
+
+  console.log(user, nextUrl.pathname);
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
